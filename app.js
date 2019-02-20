200465624cccff57d0ab60a2b198b298cf065eea
chris <gardner@gmx.net>
2019-02-20 14:15:15 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+var fs = require('fs');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+/* LOCALE MIDDLEWARE */
+app.use(function (req, res, next) {
+  if (!req.session.locale) req.session.locale = "en";
+  if (req.query.locale) req.session.locale = req.query.locale;
+  const file = "./i18n/" + req.session.locale + ".json";
+  console.log(file);
+  fs.readFile(file, (err, data) => {
+    if (err) res.send("Error loading language file: " + file);
+    else {
+      global.i18n = JSON.parse(data);
+      next();
+    }
+  });
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
